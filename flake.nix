edddfa7b231a3346aebd9113db20cd9c0b14d6b9
jowi-dev <joey8williams@gmail.com>
2024-05-24 16:37:48 -0400

    make package installable

--- flake.nix
+++ flake.nix
@@ -5,105 +5,27 @@
   };
   outputs = { self, nixpkgs, flake-utils, }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; }; 
-      in with pkgs; {
-        devShells.default = mkShell {
-#          LD_LIBRARY_PATH = "$LD_LIBRARY_PATH:${
-#              with pkgs;
-#              pkgs.lib.makeLibraryPath [
-#                # for raylib and openGL
-#                pkgs.libGL
-#                pkgs.xorg.libX11
-#                pkgs.xorg.libXi
-#
-#                # for SDL and SDL2
-#                pkgs.SDL2
-#                pkgs.SDL2_mixer
-#                pkgs.SDL
-#
-#                # for vulkan and GLFW
-#                pkgs.vulkan-loader
-#                pkgs.glfw
-#              ]
-#            }";
+    let 
+      pkgs = import nixpkgs { inherit system; }; 
+      buildInputs = with pkgs; [odin openssl sqlite curl];
+    in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = buildInputs ++  [ pkgs.valgrind ];
+        };
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "banj-cli";
+          version = "0.0.0";
+          src = ./.;
+          inherit buildInputs;
 
-          buildInputs = [
-            pkgs.odin
-            # Making requests
-            pkgs.openssl
-            # Saving AI responses
-            sqlite
-            # Making requests
-            curl
-            valgrind
-            # TODO - I want to get this working
-            # https://odin-lang.org/docs/install/#for-macos
-#            (odin.overrideAttrs (finalAttr: prevAttr: {
-#
-#              src = fetchFromGitHub {
-#                owner = "odin-lang";
-#                repo = "Odin";
-#                rev = "dev-2023-11"; # version of the branch
-#                hash = "sha256-5plcr+j9aFSaLfLQXbG4WD1GH6rE7D3uhlUbPaDEYf8=";
-#                # name = "${finalAttr.pname}-${finalAttr.version}"; # not gona work .
-#              };
-#
-#              preBuild = ''
-#
-#                echo "# for use of STB libraries"
-#                cd vendor/stb/src
-#                make
-#                cd ../../..
-#
-#              '';
-#
-#            }))
+          buildPhase = ''
+            odin build .
+          '';
+          installPhase = ''
+            mkdir -p $out/bin
+            mv banj-cli $out/bin/banj && chmod +x $out/bin/banj
+          '';
 
-#            # SDL
-#            pkgs.SDL2
-#            pkgs.SDL2_mixer
-#            pkgs.SDL
-#
-#            # GLFW
-#            pkgs.glfw
-#
-#            # vulkan
-#            pkgs.vulkan-headers
-#            pkgs.vulkan-loader
-#            pkgs.vulkan-tools
-#
-#            # x11 and raylib stuff
-#            pkgs.glxinfo
-#            pkgs.lld
-#            pkgs.gnumake
-#            pkgs.xorg.libX11.dev
-#            pkgs.xorg.libX11
-#            pkgs.xorg.libXft
-#            pkgs.xorg.libXi
-#            pkgs.xorg.libXinerama
-#            pkgs.libGL
-
-            ## not need because of vendor
-            # stb
-            # lua
-
-            # TODO - set these up so others can contribute
-            # debugging stuff and profile
-#            pkgs.valgrind
-#            pkgs.rr
-#            pkgs.gdb
-#            pkgs.lldb
-#            pkgs.gf
-#
-#            # Graphics Debugger
-#            pkgs.renderdoc
-#
-#
-#            # needed for raylib
-#            pkgs.xorg.libXcursor
-#            pkgs.xorg.libXrandr
-#            pkgs.xorg.libXinerama
-          ];
         };
       });
 }
